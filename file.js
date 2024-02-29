f98c2fc1d3409db13d240c43adbe2d33952bbb9b
Alvin Kurniawan <kurniawanalvin351@gmail.com>
2024-02-29 08:49:09 +0700

    Make operate function

--- file.js
+++ file.js
@@ -10,3 +10,16 @@ function multiply (a, b) {
 function divide (a, b) {
   return (a / b * 10) / 10;
 }
+
+function operate(operator, number1, number2) {
+  switch (operator) {
+    case "+":
+      return operate(number1, number2);
+    case "-":
+      return operate(number1, number2);
+    case "*":
+      return operate(number1, number2);
+    case "/":
+      return operate(number1, number2);
+  }
+}
